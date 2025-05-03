fb86a488f60130c556568087e8003e06200db5af
Charudatta Jadhav <cjawsome999@gmail.com>
2025-05-03 11:31:37 +0530

    adding lru header

--- LRU/inc/Lru.hpp
+++ LRU/inc/Lru.hpp
@@ -1,7 +1,12 @@
 #ifndef HEADER_LRU
 #define HEADER_LRU
 
-#include"DoublelyLinkedList.hpp"
+#include "DoublelyLinkedList.hpp"
+#include "DoubleLL.hpp"
+
+#include <unordered_map>
+#include <memory>
+#include <string>
 
 namespace LRU
 {
@@ -9,19 +14,24 @@ namespace LRU
     class Lru
     {
     private:
-        
+        size_t capacity_;
+        std::unordered_map<int, LinkedList::DoubleLL *> index_;
+        std::unique_ptr<LinkedList::DoublelyLinkedList> usageOrder_;
+        bool Evict();
+        bool Remove(int key);
+        bool HasCapacity();
+        bool IsPresent(int key) const;
+        bool IsPresent(std::string& value) const;
+        bool Get(int key, std::string& value);
     public:
-        Lru();
+        const std::string Get(int key);
+        
+        bool Put(int key, const std::string& value);
+        size_t GetSize();
+        void Clear();
+
+        Lru(size_t capacity);
         ~Lru();
     };
-    
-    Lru::Lru()
-    {
-    }
-    
-    Lru::~Lru()
-    {
-    }
-    
 }
 #endif
