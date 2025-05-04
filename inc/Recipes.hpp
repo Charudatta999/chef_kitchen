f25922a4032cdbae1bf237df341b97c43ceacdfd
Charudatta Jadhav <cjawsome999@gmail.com>
2025-05-05 01:49:13 +0530

    implemented functions of Recipes.cpp

--- inc/Recipes.hpp
+++ inc/Recipes.hpp
@@ -5,26 +5,33 @@
 #include <unordered_map>
 namespace Resturant
 {
-class Recipes
-{
-private:
-    size_t idCount_;
-    struct Recipe
+    class Recipes
     {
-        size_t id_;
-        std::string name_;
-        std::unordered_map<std::string, size_t> ingredients_;
-    };
-    std::unordered_map<size_t, Recipe> galacticCookBook_;
-    std::unordered_map<std::string, size_t> recipeLookup_;
-public:
-    Recipes();
-    ~Recipes() = default;
+    private:
+        size_t idCount_;
+        struct Recipe
+        {
+            size_t id_;
+            std::string name_;
+            std::unordered_map<std::string, size_t> ingredients_;
 
-    bool DoWeHaveTheRecipe(std::string recipeName) const;
-    std::unordered_map<std::string, size_t> GetIngredientList(std::string recipie);
-    bool AddRecipe(std::string recipeName, std::unordered_map<std::string, int> ingredients);
-    bool RemoveRecipe(std::string recipeName);
-};
+            Recipe(const std::string &name, size_t id, const std::unordered_map<std::string, size_t> &ingredients) : id_(id), name_(name), ingredients_(ingredients)
+            {
+            }
+        };
+        std::unordered_map<size_t, Recipe> galacticCookBook_;
+        std::unordered_map<std::string, size_t> recipeLookup_;
+
+        const size_t GetId(const std::string &recipeName) const;
+
+    public:
+        Recipes();
+        ~Recipes() = default;
+
+        bool DoWeHaveTheRecipe(const std::string &recipeName) const;
+        const bool GetIngredientList(std::string recipeName, std::unordered_map<std::string, size_t> &ingredientList);
+        bool AddRecipe(const std::string &recipeName, const std::unordered_map<std::string, size_t> &ingredients);
+        bool RemoveRecipe(const std::string& recipeName);
+    };
 }
 #endif
---
+++ src/Recipes.cpp
@@ -0,0 +1,72 @@
+#include "Recipes.hpp"
+#include <iostream>
+
+namespace Resturant
+{
+    Recipes::Recipes() : idCount_(0)
+    {
+    }
+
+    bool Recipes::AddRecipe(const std::string &name, const std::unordered_map<std::string, size_t> &ingredients)
+    {
+        if (DoWeHaveTheRecipe(name))
+        {
+            std::cerr << "We already have a recipe with same name" << std::endl;
+            return false;
+        }
+        Recipe recipe = Recipe(name, ++idCount_, ingredients);
+        galacticCookBook_[idCount_] = recipe;
+        recipeLookup_[name] = idCount_;
+        return true;
+    }
+
+    const size_t Recipes::GetId(const std::string &recipeName) const
+    {
+        if (recipeLookup_.count(recipeName) > 0)
+            return recipeLookup_.at(recipeName);
+        return 0;
+    }
+
+    bool Recipes::DoWeHaveTheRecipe(const std::string &recipeName) const
+    {
+        size_t id = GetId(recipeName);
+        if (id > 0)
+        {
+            if (galacticCookBook_.count(id) > 0)
+            {
+                return true;
+            }
+        }
+        std::cerr << "Recipe not present" << std::endl;
+        return false;
+    }
+
+    bool Recipes::RemoveRecipe(const std::string& recipeName)
+    {
+        if (!DoWeHaveTheRecipe(recipeName))
+        {
+            std::cerr << "Recipe not present" << std::endl;
+            return false;
+        }
+        auto id = recipeLookup_[recipeName];
+        recipeLookup_.erase(recipeName);
+        galacticCookBook_.erase(id);
+        return true;
+        
+    }
+
+    const bool Recipes::GetIngredientList(std::string recipeName, std::unordered_map<std::string, size_t> &ingredientList)
+    {
+        if (!DoWeHaveTheRecipe(recipeName))
+        {
+            std::cerr << "Recipe not present" << std::endl;
+            return false;
+        }
+
+        auto id = recipeLookup_[recipeName];
+        Recipe recipe = galacticCookBook_[id];
+        ingredientList = recipe.ingredients_;
+
+        return true;
+    }
+}
