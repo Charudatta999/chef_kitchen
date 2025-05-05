a50fbd3cad97e1048adad28c40a9fde0f10af7a8
Charudatta Jadhav <cjawsome999@gmail.com>
2025-05-05 23:19:23 +0530

    Declared Order structure

--- inc/Order.hpp
+++ inc/Order.hpp
@@ -3,20 +3,12 @@
 
 #include <string>
 #include <chrono>
-/*
-    int id
-    -string recipeName
-    -int quantity
-    -string status
-    -datetime placedTime
-    +void setStatus(status: string)
-    +bool isComplete()
-*/
 
 namespace Restaurant
 {
     enum OrderStatus
     {
+        InQueue,
         Accepted,
         Cooking,
         Cancelled,
@@ -31,24 +23,29 @@ namespace Restaurant
         std::string recipeName_;
         size_t quantity_;
         OrderStatus status_;
-        std::chrono::steady_clock::time_point placedTime_;
+        std::chrono::system_clock::time_point placedTime_;
     public:
         //Constructor
-        Order(size_t id, std::string recipeName, size_t quantity, std::string status);
+        Order(size_t id, std::string recipeName, size_t quantity, OrderStatus status);
         //Non Copyable
         Order(const Order&) = delete;
         Order& operator=(const Order&) = delete;
         //Non Moveable
-        Order(const Order&&) = delete;
-        Order& operator=(const Order&&) = delete;
+        Order(Order&&) = delete;
+        Order& operator=(Order&&) = delete;
 
         //Destructor
         ~Order() = default;
 
         //Public API's
-        const std::string GetStatus()const;
+        OrderStatus GetStatus()const;
         bool IsComplete() const;
         void SetStatus(Restaurant::OrderStatus status);
+        const std::chrono::system_clock::time_point GetOrderPlacedTime() const;
+        size_t GetId() const;
+        const std::string GetRecipeName() const;
+        size_t GetQuantity() const;
+        
     };
 }
 #endif
