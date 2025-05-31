d2ad7826e70bf7a6da83490ac43290ca159fd5c5
Charudatta Jadhav <cjawsome999@gmail.com>
2025-06-01 01:36:24 +0530

    making inventory generic

--- src/restaurant/Inventory.cpp
+++ src/restaurant/Inventory.cpp
@@ -1,19 +1,48 @@
-#include "Inventory.hpp"
+#include<iostream>
 
-namespace restaurant
+#include "Inventory.hpp"
+#include "StringUtils.hpp"
+namespace inventory
 {
     Inventory::Inventory()
     {
         cache_ = std::make_unique<LRU::Lru>(23);
-        
     }
 
-    bool Inventory::AddIngredient(Ingredient ingredient)
+    bool Inventory::IfItemExists(std::string& item) const
     {
-        
-        stockMap_[ingredient.name_];
-
+        if(stockMap_.find(item) != nullptr)
+        {
+            return true;
+        }
+        return false;
     }
-    
+    bool Inventory::AddItem(std::string& itemName,Item& itemDetails)
+    {
+        if(IfItemExists(itemName))
+        {
+            bool deatilsMatch{false};
+            std::cout<< "Item already Exits" << std::endl;
+            std::cout<< "checking for details" << std::endl;
+            auto prevItemdetails = stockMap_[itemName];
+
+            for(auto it : prevItemdetails)
+            {
+                if(it == itemDetails)
+                {
+                    std::cout << "Details are same. updating existig record with current details" << std::endl;
+                    deatilsMatch = true;
+                    break;
+                }
+
+            }
+            if(deatilsMatch)
+            {
+                it.totalQuantity_ += itemDetails.totalQuantity_;
+            }
+
+
+        }
+    }
 
 }
