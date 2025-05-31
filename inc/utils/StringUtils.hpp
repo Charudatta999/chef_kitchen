affa04bba8905058b0f476da5cce5df4b8d9c5f6
Charudatta Jadhav <cjawsome999@gmail.com>
2025-06-01 01:33:44 +0530

    refactoring

---
+++ inc/utils/StringUtils.hpp
@@ -0,0 +1,13 @@
+#ifndef STRING_UTIL_UUID_GEN_HEADER
+#define STRING_UTIL_UUID_GEN_HEADER
+
+#include <Poco/UUIDGenerator.h>
+
+namespace util
+{
+
+/// @brief Genrates a random UUID using poco
+/// @return uuid string std::string format
+std::string GetUUID();
+}
+#endif
--- inc/utils/UUIDgenrator.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef UTIL_UUID_GEN_HEADER
-#define UTIL_UUID_GEN_HEADER
-namespace UTIL
-{
-  
-}
-#endif
